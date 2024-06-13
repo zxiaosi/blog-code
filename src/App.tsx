8e12212194d57e0f231be9194fcbfebed8cf3662
zxiaosi <2692159310@qq.com>
2024-06-13 23:31:26 +0800

    添加初始化值

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,35 @@
-import { useEffect, useLayoutEffect, useRef, useState } from 'react';
-import './App.css';
-import CustomEditor from './components/CustomEditor';
-import { Editor as TinyMCEEditor } from 'tinymce';
-import useInterval from './hooks/useInterval';
+import { useCallback, useEffect, useLayoutEffect, useRef, useState } from "react";
+import "./App.css";
+import CustomEditor from "./components/CustomEditor";
+import { Editor as TinyMCEEditor } from "tinymce";
+import useInterval from "./hooks/useInterval";
 
 /** 定时器 */
 let timer = null as any;
 
 /** iframe的url */
-let iframeUrl = '';
+let iframeUrl = "";
 
 /** iframe 上的编辑按钮 */
 let iframeEditBtn: Element | undefined;
 
 /** 大文本字符 */
-const largeText = '<div>我是文本</div>'.repeat(7000);
+const largeText = "<div>我是文本</div>".repeat(7000);
 
 /** 大文本分隔符 */
-const largeTextSeparator = '<!-- largeTextSeparator -->';
+const largeTextSeparator = "<!-- largeTextSeparator -->";
 
 function App() {
   const editorRef = useRef<TinyMCEEditor>(null);
 
-  const originalValue = useRef(''); // 存储原始信息
+  const originalValue = useRef(""); // 存储原始信息
 
-  const [value, setValue] = useState(''); // 富文本的内容
+  const [initValue, setInitValue] = useState(""); // 初始化富文本内容
 
   /** 组件卸载事件 */
   const handleUnmount = () => {
     // 移出监听事件
-    iframeEditBtn?.removeEventListener('click', () => {});
+    iframeEditBtn?.removeEventListener("click", () => {});
 
     // 释放iframe的url
     URL.revokeObjectURL(iframeUrl);
@@ -40,12 +40,12 @@ function App() {
 
   /** 设置 富文本 大文本样式 */
   const handleLargeText = (body: string) => {
-    if (!body) return '';
+    if (!body) return "";
 
     // 包含标签等长度
     if (body.length > 100000) {
       // 将html字符串转换成blob对象
-      const blob = new Blob([body], { type: 'text/html; charset=utf-8' });
+      const blob = new Blob([body], { type: "text/html; charset=utf-8" });
 
       // 将blob对象转换成url
       iframeUrl = URL.createObjectURL(blob);
@@ -59,7 +59,7 @@ function App() {
 
         ${largeTextSeparator}
         
-        <div class='reply-content-ref'>
+        <div id='largeText' class='reply-content-ref'>
           <div 
             style='
               height: 52px;
@@ -102,6 +102,7 @@ function App() {
             '
             contenteditable="false"
             src=${iframeUrl}
+            loading='lazy'
           >
           </iframe>
         </div>  
@@ -115,59 +116,55 @@ function App() {
 
   /** 初始化页面 - 设置富文本 */
   useEffect(() => {
-    setValue(handleLargeText(largeText));
+    // 1.使用 ref + initialValue
+    setInitValue(largeText);
+
+    // 2.使用 handleLargeText + useLayoutEffect
+    // setInitValue(handleLargeText(largeText));
   }, []);
 
   /** 初始化页面 - 超大文本 iframe 添加点击事件 */
-  useLayoutEffect(() => {
-    // 延迟执行, 等待 dom 加载完成
-    timer = setTimeout(() => {
-      // 获取富文本的内容
-      const editorBody = editorRef.current?.getBody();
+  // useLayoutEffect(() => {
+  //   // 延迟执行, 等待 dom 加载完成
+  //   timer = setTimeout(() => {
+  //     // 获取富文本的内容
+  //     const editorBody = editorRef.current?.getBody();
 
-      // 获取iframe上的编辑按钮, 并添加监听事件
-      iframeEditBtn =
-        editorBody?.getElementsByClassName('editor-quote-btn')?.[0];
-      if (!iframeEditBtn) return; // 没有编辑按钮则不执行
+  //     // 获取iframe上的编辑按钮, 并添加监听事件
+  //     iframeEditBtn = editorBody?.getElementsByClassName("editor-quote-btn")?.[0];
+  //     if (!iframeEditBtn) return; // 没有编辑按钮则不执行
 
-      iframeEditBtn?.addEventListener?.('click', function (event) {
-        event.preventDefault(); // 阻止默认链接行为，避免跳转
+  //     iframeEditBtn?.addEventListener?.("click", function (event) {
+  //       event.preventDefault(); // 阻止默认链接行为，避免跳转
 
-        const newText = editorBody?.innerHTML.split(largeTextSeparator)?.[0]; // 获取富文本的内容
-        setValue(newText + originalValue.current); // 设置新的值
-        originalValue.current = ''; // 清空原始信息
-        handleUnmount(); // 组件卸载事件
-      });
+  //       const newText = editorBody?.innerHTML.split(largeTextSeparator)?.[0]; // 获取富文本的内容
+  //       editorRef.current?.setContent(newText + originalValue.current); // 设置新的值
+  //       originalValue.current = ""; // 清空原始信息
+  //       handleUnmount(); // 组件卸载事件
+  //     });
 
-      // 隐藏 iframe 中的 span 遮罩层 (防止不能滚动)
-      const iframeMask = editorBody?.getElementsByClassName?.('mce-shim')?.[0];
-      if (iframeMask) iframeMask.style.visibility = 'hidden';
-    }, 1000);
+  //     // 隐藏 iframe 中的 span 遮罩层 (防止不能滚动)
+  //     const iframeMask = editorBody?.getElementsByClassName?.("mce-shim")?.[0];
+  //     if (iframeMask) iframeMask.style.visibility = "hidden";
+  //   }, 1000);
 
-    return () => handleUnmount();
-  }, []);
+  //   return () => handleUnmount();
+  // }, []);
 
   /** 提交事件 */
-  const handleSave = (e?: any) => {
+  const handleSave = useCallback((e?: any) => {
     // ctrl + s 保存时获取不到 editorContent, 故从参数中获取
-    const realValue = e?.getContent?.() || value;
-    const mergetText = originalValue.current
-      ? realValue.split(largeTextSeparator)?.[0] + originalValue.current
-      : realValue;
-    console.log('%c 提交的内容: ', 'color: red', mergetText);
-  };
+    const realValue = editorRef.current?.getContent();
+    const mergetText = originalValue.current ? realValue?.split(largeTextSeparator)?.[0] + originalValue.current : realValue;
+    console.log("%c 提交的内容: ", "color: red", mergetText);
+  }, []);
 
   // 每60s自动保存
   // useInterval(handleSave, 60000);
 
   return (
     <div className="page">
-      <CustomEditor
-        editorRef={editorRef}
-        value={value}
-        onChangeValue={setValue}
-        onEditorSave={handleSave}
-      />
+      <CustomEditor editorRef={editorRef} initialValue={initValue} onEditorSave={handleSave} />
     </div>
   );
 }
--- src/components/CustomEditor/index.tsx
+++ src/components/CustomEditor/index.tsx
@@ -1,7 +1,7 @@
-import { Editor } from '@tinymce/tinymce-react';
-import React from 'react';
-import axios from 'axios';
-import { Editor as TinyMCEEditor } from 'tinymce';
+import { Editor } from "@tinymce/tinymce-react";
+import React from "react";
+import axios from "axios";
+import { Editor as TinyMCEEditor } from "tinymce";
 
 interface BlobInfo {
   id: () => string;
@@ -17,13 +17,15 @@ interface Props {
   /** ref */
   editorRef?: any;
   /** 富文本内容 */
-  value: string;
+  value?: string;
   /** 富文本内容改变事件 */
   onChangeValue?: (value: string) => void;
   /** 富文本高度 */
   height?: number | string;
   /** 保存事件 */
   onEditorSave?: (editor: TinyMCEEditor) => void;
+  /** 初始化值 */
+  initialValue?: string;
 }
 
 /**
@@ -37,22 +39,16 @@ interface Props {
  * 关于复制粘贴: https://www.tiny.cloud/docs/tinymce/6/introduction-to-powerpaste/
  */
 const Index: React.FC<Props> = (props) => {
-  const {
-    editorRef,
-    value,
-    onChangeValue,
-    height = '500px',
-    onEditorSave,
-  } = props;
+  const { editorRef, value, onChangeValue, height = "500px", onEditorSave, initialValue } = props;
 
   /** 图片上传 */
   const handleUpload = async (blobInfo: BlobInfo): any => {
     return new Promise((resolve, reject) => {
       const formData = new FormData();
-      formData.append('file', blobInfo.blob());
-      axios.post('localhost:3000/api/upload', formData).then((data: any) => {
+      formData.append("file", blobInfo.blob());
+      axios.post("localhost:3000/api/upload", formData).then((data: any) => {
         if (data.url) resolve(data.url);
-        else reject('上传失败');
+        else reject("上传失败");
       });
     });
   };
@@ -64,64 +60,65 @@ const Index: React.FC<Props> = (props) => {
   };
 
   return (
-    <div className="editor" style={{ height, minHeight: 500, width: '100%' }}>
+    <div className="editor" style={{ height, minHeight: 500, width: "100%" }}>
       <Editor
         value={value}
-        tinymceScriptSrc={'/tinymce/tinymce.min.js'} // 防止需要登录才能访问的情况
+        initialValue={initialValue}
+        tinymceScriptSrc={"/tinymce/tinymce.min.js"} // 防止需要登录才能访问的情况
         init={{
-          height: '100%',
+          height: "100%",
           menubar: false, // 是否显示菜单栏
           contextmenu: false, // 是否显示右键菜单
-          language: 'zh-Hans', // 语言
+          language: "zh-Hans", // 语言
           paste_data_images: true, // 是否粘贴图片
           remove_trailing_brs: true, // 删除尾部 br
-          line_height_formats: '1 1.2 1.4 1.6 1.8 2.0', // 行高
-          placeholder: 'Ctrl + S 可保存邮件到草稿箱', // 占位符
+          line_height_formats: "1 1.2 1.4 1.6 1.8 2.0", // 行高
+          placeholder: "Ctrl + S 可保存邮件到草稿箱", // 占位符
           // save_enablewhendirty: false, // 是否在内容无变化时启用保存按钮(慎用)
           add_form_submit_trigger: true, // 添加表单提交触发器 ctrl+s
           save_onsavecallback: onEditorSave, // 保存回调
-          toolbar_mode: 'sliding', // 工具栏 更多按钮 的模式
-          forced_root_block: 'div', // 强制根节点为 div (回车符号, 间距过大问题)
-          paste_webkit_styles: 'all', // 保留所有 webkit 粘贴样式 eg: https://www.tiny.cloud/docs/tinymce/6/copy-and-paste/#paste_webkit_styles
+          toolbar_mode: "sliding", // 工具栏 更多按钮 的模式
+          forced_root_block: "div", // 强制根节点为 div (回车符号, 间距过大问题)
+          paste_webkit_styles: "all", // 保留所有 webkit 粘贴样式 eg: https://www.tiny.cloud/docs/tinymce/6/copy-and-paste/#paste_webkit_styles
           help_accessibility: true, // 帮助按钮是否可访问
           convert_urls: false, // TinyMCE 默认会将图片的 url 转换为相对路径, 这里禁用 (取得是 img 中 data-mce-src 的值)
           images_upload_handler: handleUpload, // 图片上传
           plugins: [
-            'lists', // 列表 - bullist numlist
-            'advlist', // 高级列表 - bullist numlist
-            'charmap', // 字符映射表 - charmap
-            'code', // 代码 - code
-            'directionality', // 从左边/右边输入 - ltr rtl
-            'emoticons', // 表情符号 - emoticons
-            'fullscreen', // 全屏 - fullscreen
-            'help', // 帮助(一些快捷键和编辑器信息) - help
-            'image', // 图片 - image
-            'insertdatetime', // 插入日期时间 - insertdatetime
-            'link', // 链接 - link
-            'media', // 媒体 - media
-            'nonbreaking', // 不间断空格实体(&nbsp;) - nonbreaking
-            'pagebreak', // 分页符 - pagebreak
-            'preview', // 预览 - preview
-            'save', // 保存 - save
-            'table', // 表格 - table
-            'visualblocks', // 显示块级元素边框 - visualblocks
-            'visualchars', // 显示不可见字符 - visualchars
-            'wordcount', // 字数统计 - wordcount
+            "lists", // 列表 - bullist numlist
+            "advlist", // 高级列表 - bullist numlist
+            "charmap", // 字符映射表 - charmap
+            "code", // 代码 - code
+            "directionality", // 从左边/右边输入 - ltr rtl
+            "emoticons", // 表情符号 - emoticons
+            "fullscreen", // 全屏 - fullscreen
+            "help", // 帮助(一些快捷键和编辑器信息) - help
+            "image", // 图片 - image
+            "insertdatetime", // 插入日期时间 - insertdatetime
+            "link", // 链接 - link
+            "media", // 媒体 - media
+            "nonbreaking", // 不间断空格实体(&nbsp;) - nonbreaking
+            "pagebreak", // 分页符 - pagebreak
+            "preview", // 预览 - preview
+            "save", // 保存 - save
+            "table", // 表格 - table
+            "visualblocks", // 显示块级元素边框 - visualblocks
+            "visualchars", // 显示不可见字符 - visualchars
+            "wordcount", // 字数统计 - wordcount
           ],
           toolbar:
-            'undo redo removeformat | blocks fontfamily fontsize | bold italic underline |' +
-            'forecolor backcolor | align lineheight | link emoticons table |' +
-            'bullist numlist | preview fullscreen | outdent indent |' +
-            'superscript subscript strikethrough blockquote charmap | ltr rtl |' +
-            'visualchars visualblocks | hr nonbreaking pagebreak |' +
-            'cut copy paste pastetext | code print insertdatetime |' +
-            'image media wordcount help',
-          font_css: '/tinymce/font/font.css', // 引入自定义字体, 详见: /public/tinymce/font/font.css
+            "undo redo removeformat | blocks fontfamily fontsize | bold italic underline |" +
+            "forecolor backcolor | align lineheight | link emoticons table |" +
+            "bullist numlist | preview fullscreen | outdent indent |" +
+            "superscript subscript strikethrough blockquote charmap | ltr rtl |" +
+            "visualchars visualblocks | hr nonbreaking pagebreak |" +
+            "cut copy paste pastetext | code print insertdatetime |" +
+            "image media wordcount help",
+          font_css: "/tinymce/font/font.css", // 引入自定义字体, 详见: /public/tinymce/font/font.css
           font_family_formats:
-            '微软雅黑=微软雅黑,宋体,sans-serif;' + //
-            'Times New Roman=times new roman,times;' + //
-            'Comic Sans MS=comic sans ms,sans-serif;' + //
-            '自定义字体=custom font;', // 自定义字体(因为stackblitz单个文件不能大于1MB, 并没有实际引入, 实际使用时引入即可)
+            "微软雅黑=微软雅黑,宋体,sans-serif;" + //
+            "Times New Roman=times new roman,times;" + //
+            "Comic Sans MS=comic sans ms,sans-serif;" + //
+            "自定义字体=custom font;", // 自定义字体(因为stackblitz单个文件不能大于1MB, 并没有实际引入, 实际使用时引入即可)
           content_style: `body { font-family:"微软雅黑"; }`,
         }}
         onEditorChange={handleEditorChange}

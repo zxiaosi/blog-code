eeb5ab44e38f7ef6f66e5a11b4ea81a2ab0e1b71
zxiaosi <2692159310@qq.com>
2024-11-10 19:02:19 +0800

    上传测试代码

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,49 @@
+import { Suspense, useState } from 'react';
 import './App.css';
+import { QueryClient, QueryClientProvider, useSuspenseQuery } from '@tanstack/react-query';
 
 function App() {
-  return <>Hello, World</>;
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <QueryClientProvider client={new QueryClient()}>
+      <>
+        <button onClick={() => setIsOpen((x) => !x)}>toggle</button>
+        {isOpen && (
+          <Suspense fallback="loading...">
+            <Thing1 />
+            <Thing2 />
+          </Suspense>
+        )}
+      </>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
+
+function Thing1() {
+  const query = useSuspenseQuery({
+    queryKey: ['thing1'],
+    queryFn: async () => {
+      console.log('start loading thing1');
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log('done loading thing1');
+      return 'Thing1';
+    },
+  });
+  return <div>{query.data}</div>;
+}
+
+function Thing2() {
+  const query = useSuspenseQuery({
+    queryKey: ['thing2'],
+    queryFn: async () => {
+      console.log('start loading thing2');
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      console.log('done loading thing2');
+      return 'Thing2';
+    },
+  });
+  return <div>{query.data}</div>;
+}

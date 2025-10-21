a1b7e4162c4c2d73994f66788206547a936e2b7c
zxiaosi <2692159310@qq.com>
2025-10-22 00:48:32 +0800

    feat: 配置路由信息

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,37 @@
+import About from '@/pages/about';
+import Home from '@/pages/home';
+import BaseLayout from '@/pages/layout';
+import { ConfigProvider } from 'antd';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
+
+/** 路由配置 */
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="/home" />,
+  },
+  {
+    path: '/',
+    element: <BaseLayout />,
+    children: [
+      {
+        path: '/home',
+        element: <Home />,
+      },
+      {
+        path: '/about',
+        element: <About />,
+      },
+    ],
+  },
+]);
+
 function App() {
-  return <>Monitor</>;
+  return (
+    <ConfigProvider>
+      <RouterProvider router={router} />
+    </ConfigProvider>
+  );
 }
 
 export default App;

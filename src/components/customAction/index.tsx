92779eb9c8c4b720cb51a2ec32a48c3f3e8f352b
zxiaosi <2692159310@qq.com>
2025-10-22 03:47:53 +0800

    feat: 添加导入导出方法

--- src/components/customAction/index.tsx
+++ src/components/customAction/index.tsx
@@ -1,10 +1,18 @@
 import { useLogData } from '@/hooks';
-import { clearDataUtil } from '@/monitor/utils';
+import { clearDataUtil, exportDataUtil } from '@/monitor/utils';
 import { Button, Space } from 'antd';
 import './index.less';
 
 const CustomAction = () => {
   const getData = useLogData((state) => state.getData);
+
+  /** 导入数据 */
+  const handleImportData = () => {};
+
+  /** 导出数据 */
+  const handleExportData = async () => {
+    await exportDataUtil();
+  };
 
   /** 清空数据 */
   const handleClearData = async () => {
@@ -15,8 +23,10 @@ const CustomAction = () => {
   return (
     <div className="custom-action">
       <Space wrap>
-        <Button type="primary">导入数据</Button>
-        <Button type="primary">导出数据</Button>
+        <Button type="primary" onClick={handleImportData}>导入数据</Button>
+        <Button type="primary" onClick={handleExportData}>
+          导出数据
+        </Button>
         <Button type="primary" danger onClick={handleClearData}>
           清空数据
         </Button>

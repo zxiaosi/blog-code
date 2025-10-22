8799d4d8ccac5b9461fa437380946ecf8858fb0b
zxiaosi <2692159310@qq.com>
2025-10-22 23:40:18 +0800

    feat: about 页面添加错误示例

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,5 +1,20 @@
+import { Button } from 'antd';
+
 const About = () => {
-  return <div>About Page</div>;
+  /** js错误 */
+  const handleJsError = () => {
+    var num = new Number(12.34);
+    console.log(num.toFixed(-1));
+  };
+
+  return (
+    <div>
+      <h1>This is an about page</h1>
+      <Button type="primary" onClick={handleJsError}>
+        js错误
+      </Button>
+    </div>
+  );
 };
 
 export default About;
